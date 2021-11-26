b434011ffc8bdf853076eb9fb59bb4943e327159
Daniel <daniel.andres.ramos.v@gmail.com>
2021-11-26 14:45:40 -0300

    first commit

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,20 +1,17 @@
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&limit=10&api_key=lHLSFIqzWGT840N10EA9PKW00gyxhP9s`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
 
-export const getGifs = async ( category ) => {
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url,
+    };
+  });
 
-        const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=lHLSFIqzWGT840N10EA9PKW00gyxhP9s`
-        const resp = await fetch ( url );
-        const {data} = await resp.json();
-
-        const gifs = data.map ( img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-
-        })
-
-       return gifs;
-      
-
-    }
+  return gifs;
+};
